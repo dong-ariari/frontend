6eec1cddb862962b456469b6995258938a203f54
yeoniii20 <commetoi_yeoni@naver.com>
2024-04-29 23:50:23 +0900

    feat: page에 input component 적용

--- src/pages/offcampus.jsx
+++ src/pages/offcampus.jsx
@@ -1,7 +1,12 @@
+import Input from "../components/input/input";
+
 const Offcampus = () => {
   return (
     <>
-      <div>Offcampus</div>
+      <div style={{ backgroundColor: "#ffe8fb", padding: 100 }}>
+        <Input title={"동아리 이름"} placeholder={"동아리 이름을 입력해주세요"}/>
+        <Input title={"교내 동아리"} placeholder={"교내 동아리"}/>
+      </div>
     </>
   );
 };
