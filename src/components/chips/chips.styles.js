11e04447c7b1eb9802d86a7c76dfe264a47dddeb
yeoniii20 <commetoi_yeoni@naver.com>
2024-04-29 21:46:31 +0900

    fix: variant true로 변경

--- src/components/chips/chips.styles.js
+++ src/components/chips/chips.styles.js
@@ -11,14 +11,16 @@ export const Title = styled.p`
   padding-top: 0.5rem;
   padding-bottom: 0.5rem;
   border-radius: 1rem;
-  background-color: ${(props) =>
-    props.variant === "dark"
+  background-color: ${(props) => {
+    return props.$variant === true
       ? props.theme.color.grey._800
-      : props.theme.color.grey._200};
-  color: ${(props) =>
-    props.variant === "dark"
+      : props.theme.color.grey._200;
+  }};
+  color: ${(props) => {
+    return props.$variant === true
       ? props.theme.color.white
-      : props.theme.color.grey._600};
+      : props.theme.color.grey._600;
+  }};
   ${(props) => props.theme.typo.fontSize._5};
   ${(props) => props.theme.typo.fontWeight._500};
   cursor: pointer;
