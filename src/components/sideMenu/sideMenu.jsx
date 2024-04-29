e6648b2c778cc1049bdcd8aef2c5719e0291c390
yeoniii20 <commetoi_yeoni@naver.com>
2024-04-29 22:36:52 +0900

    feat: import image

--- src/components/sideMenu/sideMenu.jsx
+++ src/components/sideMenu/sideMenu.jsx
@@ -1,11 +1,12 @@
 import * as S from "./sideMenu.styles";
+import EyeIcon from "../../assets/images/eyes.png";
 
 const SideMenu = () => {
   return (
     <>
       <S.Container>
         <S.Section>
-          <S.Image />
+          <S.Image src={EyeIcon} alt="side_menu_icon" />
           <S.Text>둘러보기</S.Text>
         </S.Section>
       </S.Container>
