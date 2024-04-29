e496323b43bdf9abffad09cedcae5124349c0d90
ltbhsf <kmukyj21@kookmin.ac.kr>
2024-04-29 23:06:34 +0900

    feat: 색상 변수 추가 & px -> rem 변경

--- src/assets/styles/DefaultTheme.js
+++ src/assets/styles/DefaultTheme.js
@@ -12,6 +12,58 @@ const DefaultTheme = {
       _700: "#404040",
       _800: "#242424",
     },
+    red: {
+      _100: "#FFEEEE",
+      _200: "#FCDFDF",
+      _300: "#FDBAB8",
+      _400: "#FA5855",
+      _500: "#EE5350",
+      _600: "#D8423F",
+      _700: "#AF3632",
+      _800: "#99322F",
+    }, 
+    yellow: {
+      _100: "#FFFBEC",
+      _200: "#FFF5D2",
+      _300: "#FCECB3",
+      _400: "#FCDF78",
+      _500: "#FAD44D",
+      _600: "#F8D042",
+      _700: "#E8BC25",
+      _800: "#DCA12F",
+    }, 
+    green: {
+      _100: "#E6F6F4",
+      _200: "#D9F2EF",
+      _300: "#B0E4DD",
+      _400: "#00A991",
+      _500: "#009883",
+      _600: "#007F6D",
+      _700: "#0D6155",
+      _800: "#1B5048",
+    }, 
+    blue: {
+      _100: "#EBF5FF",
+      _200: "#C4DBFD",
+      _300: "#8FC1FF",
+      _400: "#3D8BFF",
+      _500: "#327DE6",
+      _600: "#3569BF",
+      _700: "#1E4173",
+      _800: "#193561",
+    }, 
+    purple: {
+      _100: "#FFF0FF",
+      _200: "#FFE6FF",
+      _300: "#FFD7FF",
+      _400: "#FFC8FF",
+      _500: "#F5A0F5",
+      _600: "#E682E6",
+      _700: "#D764D7",
+      _800: "#B955B9",
+    }, 
+
+
   },
   typo: {
     fontWeight: {
@@ -22,24 +74,24 @@ const DefaultTheme = {
       _700: `font-weight: 700;`,
     },
     fontSize: {
-      _1: `font-size: 32px;
-      line-height: 42px;`,
-      _2: `font-size: 28px;
-      l3ne-height: 39px;`,
-      _3: `font-size: 26px;
-      line-height: 36px;`,
-      _4: `font-size: 20px;
-      line-height: 30px;`,
-      _5: `font-size: 18px;
-      line-height: 27px;`,
-      _6: `font-size: 16px;
-      line-height: 24px;`,
-      _7: `font-size: 14px;
-      line-height: 21px;`,
-      _8: `font-size: 12px;
-      line-height: 18px;`,
-      _9: `font-size: 10px;
-      line-height: 15px;`,
+      _1: `font-size: 2rem;
+      line-height: 2.625rem;`,
+      _2: `font-size: 1.75rem;
+      l3ne-height: 2.438rem;`,
+      _3: `font-size: 1.625rem;
+      line-height: 2.25rem;`,
+      _4: `font-size: 1.25rem;
+      line-height: 1.875rem;`,
+      _5: `font-size: 1.125rem;
+      line-height: 1.688rem;`,
+      _6: `font-size: 1rem;
+      line-height: 1.5rem;`,
+      _7: `font-size: 0.875rem;
+      line-height: 1.313rem;`,
+      _8: `font-size: 0.75rem;
+      line-height: 1.125rem;`,
+      _9: `font-size: 0.625rem;
+      line-height: 0.938rem;`,
     },
   },
 };
