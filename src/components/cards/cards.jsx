e50cf414c8d7bb029cdf0ccc99d5477e8465d6ec
ltbhsf <kmukyj21@kookmin.ac.kr>
2024-04-30 16:37:39 +0900

    feat: Layer 파일 위치 이동

--- src/components/cards/cards.jsx
+++ src/components/cards/cards.jsx
@@ -1,16 +1,20 @@
 import * as S from "./cards.styles";
 
-export default function Cards({ small, data }) {
+export default function Cards({ small, cardList }) {
   return (
-    <S.Container small={small}>
-      <S.Main>
-        <S.Poster small={small} src={data.posterData} />
-        <S.Box>
-          <S.Title>{data.clubName}</S.Title>
-          <S.Badge>Badge</S.Badge>
-        </S.Box>
-      </S.Main>
-      <S.Text>{data.recruitText}</S.Text>
-    </S.Container>
+    <S.Layer>
+      {cardList.map((item) => (
+        <S.Container small={small}>
+          <S.Main>
+            <S.Poster small={small} src={item.posterData} />
+            <S.Box>
+              <S.Title>{item.clubName}</S.Title>
+              <S.Badge>Badge</S.Badge>
+            </S.Box>
+          </S.Main>
+          <S.Text>{item.recruitText}</S.Text>
+        </S.Container>
+      ))}
+    </S.Layer>
   );
 }
--- src/components/cards/cardsComponent.jsx
+++ src/components/cards/cardsComponent.jsx
@@ -1,11 +1,5 @@
 import Cards from "./cards";
-import { Layer } from "./cards.styles";
+
 export default function CardsComponent({ small, cardList }) {
-  return (
-    <Layer>
-      {cardList.map((item) => (
-        <Cards key={item.key} small={small} data={item} />
-      ))}
-    </Layer>
-  );
+  return <Cards small={small} cardList={cardList} />;
 }
