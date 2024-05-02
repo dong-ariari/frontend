c9bd2802444df709e337e8913918a19fe1350ba4
ltbhsf <kmukyj21@kookmin.ac.kr>
2024-05-02 21:11:28 +0900

    feat: submit 방식 변경

--- src/components/search/search.jsx
+++ src/components/search/search.jsx
@@ -1,17 +1,17 @@
 import * as S from "./search.styles";
 
 import btn from "../../assets/icons/searchBtn.png";
+import { useState } from "react";
 export default function Search() {
-    function handleSubmit(e){
-        e.preventDefault();
-        console.log(e.target.value)
-
-    }
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(e.target[0].value)
+    
+  }
   return (
     <S.Container onSubmit={handleSubmit}>
-      <S.Input placeholder="관심있는 동아리를 찾을 수 있어요" />
+      <S.Input type="text" placeholder="관심있는 동아리를 찾을 수 있어요" />
       <S.Button type="submit" $url={btn}></S.Button>
     </S.Container>
   );
 }
-
