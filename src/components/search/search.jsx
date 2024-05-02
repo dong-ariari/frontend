524e9bc26e6407fcdf655bb880b3c8da8f49a14c
ltbhsf <kmukyj21@kookmin.ac.kr>
2024-05-02 17:35:47 +0900

    feat: add styles & icon img

--- src/components/search/search.jsx
+++ src/components/search/search.jsx
@@ -1,5 +1,11 @@
-import * as S from './search.styles'; 
+import * as S from "./search.styles";
 
+import btn from "../../assets/icons/searchBtn.png";
 export default function Search() {
-  return <>search</>;
+  return (
+    <S.Container>
+      <S.Input placeholder="관심있는 동아리를 찾을 수 있어요" />
+      <img src={btn} alt="search button" />
+    </S.Container>
+  );
 }
--- src/components/search/search.styles.js
+++ src/components/search/search.styles.js
@@ -1,1 +1,21 @@
 import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 36.688rem;
+  height: 3rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: solid 0.063rem ${props => props.theme.color.grey._300};
+  padding: 0 1rem 0 0.5rem; 
+
+`;
+export const Input = styled.input`
+  flex: 1;
+  height: inherit;
+  margin-bottom: 0.375rem;
+  outline: none; 
+  ${(props) => props.theme.typo.fontSize._3};
+  ${(props) => props.theme.typo.fontWeight._500};
+`;
+
