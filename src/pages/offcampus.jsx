28787bfb7b652b9365d27d3312a1c7ebe2979a29
yeoniii20 <commetoi_yeoni@naver.com>
2024-05-02 22:12:42 +0900

    feat: add button component

--- src/pages/offcampus.jsx
+++ src/pages/offcampus.jsx
@@ -1,14 +1,11 @@
+import Button from "../components/button/button";
 import Input from "../components/input/input";
 
 const Offcampus = () => {
   return (
     <>
       <div style={{ backgroundColor: "#ffe8fb", padding: 100 }}>
-        <Input
-          title={"동아리 이름"}
-          placeholder={"동아리 이름을 입력해주세요"}
-        />
-        <Input title={"교내 동아리"} placeholder={"교내 동아리"} />
+        <Button title={"제출하기"} onClick={() => {}} />
       </div>
     </>
   );
