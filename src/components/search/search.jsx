f5d0972a94e256fbc64b23937e24c4ed6e104b4a
ltbhsf <kmukyj21@kookmin.ac.kr>
2024-05-02 21:10:49 +0900

    feat: add submit function

--- src/components/search/search.jsx
+++ src/components/search/search.jsx
@@ -2,10 +2,16 @@ import * as S from "./search.styles";
 
 import btn from "../../assets/icons/searchBtn.png";
 export default function Search() {
+    function handleSubmit(e){
+        e.preventDefault();
+        console.log(e.target.value)
+
+    }
   return (
-    <S.Container>
+    <S.Container onSubmit={handleSubmit}>
       <S.Input placeholder="관심있는 동아리를 찾을 수 있어요" />
-      <img src={btn} alt="search button" />
+      <S.Button type="submit" $url={btn}></S.Button>
     </S.Container>
   );
 }
+
--- src/components/search/search.styles.js
+++ src/components/search/search.styles.js
@@ -1,21 +1,27 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
+export const Container = styled.form`
   width: 36.688rem;
   height: 3rem;
   display: flex;
   justify-content: space-between;
   align-items: center;
-  border-bottom: solid 0.063rem ${props => props.theme.color.grey._300};
-  padding: 0 1rem 0 0.5rem; 
-
+  border-bottom: solid 0.063rem ${(props) => props.theme.color.grey._300};
+  padding: 0 1rem 0 0.5rem;
 `;
 export const Input = styled.input`
   flex: 1;
   height: inherit;
   margin-bottom: 0.375rem;
-  outline: none; 
+  outline: none;
+  box-shadow: none;
   ${(props) => props.theme.typo.fontSize._3};
   ${(props) => props.theme.typo.fontWeight._500};
 `;
 
+export const Button = styled.button`
+  background: url(${(props) => props.$url}) no-repeat;
+  width: 2rem; 
+  height: 2rem;
+  border: none;
+`;
