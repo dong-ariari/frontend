b745619c1a20b0b44af722299141b8651feb4c30
ltbhsf <kmukyj21@kookmin.ac.kr>
2024-05-05 07:52:51 +0900

    feat: add svg icon

--- src/components/dropdown/dropdown.jsx
+++ src/components/dropdown/dropdown.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import * as S from "./dropdown.styles";
-import dropBtn from "../../assets/icons/dropBtn.png";
-import dropBtnA from "../../assets/icons/dropBtn-a.png";
+import dropBtn from "../../assets/icons/dropBtn.svg";
+import dropBtnA from "../../assets/icons/dropBtn-a.svg";
 export default function DropDown({
   title,
   handleSpread,
@@ -17,7 +17,8 @@ export default function DropDown({
         onClick={handleSpread}
       >
         <span>{state.selected === null ? title : state.selected.text}</span>
-        <img src={dropBtnA} />
+        
+        <img src={state.active ? dropBtnA : dropBtn} />
       </S.Head>
 
       {spreadData &&
