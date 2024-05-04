8aaacf56d736a0a864e64ad7ab5f8c7d963fe67a
ltbhsf <kmukyj21@kookmin.ac.kr>
2024-05-04 16:56:55 +0900

    feat: mv vars from dropdown to dropdownComponents

--- src/components/dropdown/dropdown.jsx
+++ src/components/dropdown/dropdown.jsx
@@ -3,26 +3,23 @@ import * as S from "./dropdown.styles";
 import dropBtn from "../../assets/icons/dropBtn.png";
 import dropBtnA from "../../assets/icons/dropBtn-a.png";
 import { DUMMY_SPREAD } from "../../data/dummy/dropdown";
-export default function DropDown({title, handleSpread}) {
-
+export default function DropDown({
+  title,
+  handleSpread,
+  isOpen,
+  selected,
+  spreadData,
+}) {
   return (
     <S.Container>
-      <S.Head
-        $active={true}
-        $isOpen={isOpen.depart}
-        onClick={handleSpread}
-      >
-        <span>
-          {selected.depart === null
-            ? title
-            : selected.depart.text}
-        </span>
+      <S.Head $active={true} $isOpen={isOpen} onClick={handleSpread}>
+        <span>{selected === null ? title : selected.text}</span>
         <img src={dropBtnA} />
       </S.Head>
 
       {spreadData &&
-        isOpen.depart &&
-        spreadData.depart.map((item) => (
+        isOpen &&
+        spreadData.map((item) => (
           <S.Spread onClick={() => handleDepartSelect(item)} key={item.id}>
             {item.text}
           </S.Spread>
--- src/components/dropdown/dropdownComponents.jsx
+++ src/components/dropdown/dropdownComponents.jsx
@@ -1,7 +1,9 @@
+import { useState } from "react";
 import DropDown from "./dropdown";
 import * as S from "./dropdown.styles";
 
 export default function DropdownComponents() {
+    
   function handleDepartSpread() {}
   function handleMajorSpread() {}
   return (
@@ -9,10 +11,16 @@ export default function DropdownComponents() {
       <DropDown
         handleSpread={handleDepartSpread}
         title={"단과대학을 선택해주세요"}
+        isOpen={()=>{}}
+        selected={{text: 'asd'}}
+        spreadData={''}
       />
       <DropDown
         handleSpread={handleMajorSpread}
         title={"학과를 선택해주세요"}
+        isOpen={()=>{}}
+        selected={{text: 'asd'}}
+        spreadData={''}
       />
     </S.Layer>
   );
