15ac3974cb499471642cbcb9881dd825e0028947
yeoniii20 <commetoi_yeoni@naver.com>
2024-05-18 21:47:06 +0900

    feat: add fn and disabled state

--- src/pages/offcampus.jsx
+++ src/pages/offcampus.jsx
@@ -9,15 +9,23 @@ const Offcampus = () => {
           title={"동아리 이름"}
           placeholder={"동아리 이름을 입력해주세요"}
           isEdit={false}
+          onValueChange={() => {}}
         />
         <Input
           title={"교내 동아리"}
           placeholder={"교내 동아리"}
           isEdit={true}
+          onValueChange={() => {}}
         />
         <Textarea
           title={"소개글"}
           placeholder={"소개하고 싶은 내용을 입력해주세요"}
+          isEdit={false}
+          max={500}
+        />
+        <Textarea
+          title={"소개글"}
+          placeholder={"편집 가능한 상태"}
           isEdit={true}
           max={500}
         />
