293ea5c416f128fa3c8a6c1aab25c75f473d1a47
ltbhsf <kmukyj21@kookmin.ac.kr>
2024-05-20 22:02:06 +0900

    feat: can search when clicking enter

--- src/components/search/search.jsx
+++ src/components/search/search.jsx
@@ -6,9 +6,15 @@ export default function Search({placeholder, onSearch}) {
     e.preventDefault();
     onSearch(e.target[0].value); 
   }
+
+  function handleKeyDwon(e){
+    if (e.key === 'Enter'){
+      onSearch(e.target.value); 
+    }
+  }
   return (
     <S.Container onSubmit={handleSubmit}>
-      <S.Input type="text" placeholder={placeholder} />
+      <S.Input type="text" placeholder={placeholder} onKeyDown={handleKeyDwon} />
       <S.Button type="submit" $url={btn}></S.Button>
     </S.Container>
   );
