be17fcfdc3415bb44890d02c79a34dc47d860b50
ltbhsf <kmukyj21@kookmin.ac.kr>
2024-05-20 22:08:25 +0900

    style: change bg color

--- src/components/header/header.styles.js
+++ src/components/header/header.styles.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const Layer = styled.div`
   display: flex;
   justify-content: center;
-  background-color: #9bf22c;
+  background-color: #FFD7FF;
 `;
 export const Container = styled.div`
   display: flex;
