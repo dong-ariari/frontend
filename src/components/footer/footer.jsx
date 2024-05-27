b489a2633675bc1e11237a744eda747baa8a8e56
ltbhsf <kmukyj21@kookmin.ac.kr>
2024-05-27 16:49:46 +0900

    style: 텍스트, 정렬 정의

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,3 +1,10 @@
-export default function Footer() {
-  return <div>footer</div>;
+import * as S from "./footer.styles";
+
+export default function Footer({ text, text2 }) {
+  return (
+    <S.Layer>
+      <S.Text>{text}</S.Text>
+      <S.Text>{text2}</S.Text>
+    </S.Layer>
+  );
 }
--- src/pages/all.jsx
+++ src/pages/all.jsx
@@ -1,17 +1,28 @@
 import Badge from "../components/badge/badge";
+import Footer from "../components/footer/footer";
 import SideMenu from "../components/sideMenu/sideMenu";
 import SideMenuComponent from "../components/sideMenu/sideMenuComponent";
 import { SIDEMENU_DATA } from "../data/components/sideMenu";
 
 const All = () => {
   return (
-    <div style={{ backgroundColor: "#d4d4d4" }}>
-      <div>all</div>
-      <div style={{ padding: 20, backgroundColor: "#aad2e8" }}>
-        <Badge title={"badge"} />
+    <>
+      <div style={{ backgroundColor: "#d4d4d4" }}>
+        <div>all</div>
+        <div style={{ padding: 20, backgroundColor: "#aad2e8" }}>
+          <Badge title={"badge"} />
+        </div>
+        <SideMenuComponent data={SIDEMENU_DATA} />
       </div>
-      <SideMenuComponent data={SIDEMENU_DATA} />
-    </div>
+      <Footer
+        text={
+          "(주) 캠퍼스크루 ㅣ 책임자: 김대선 ㅣ 등록일자: 2024년 1월 20일 ㅣ대표자: 원순재 ㅣ ALL RIGHTS RESERVED."
+        }
+        text2={
+          "주소: 서울특별시 광진구 능동로 209, 세종대왕타워 2층 ㅣ 전화번호: 02-3408-3114 ㅣ 사업자번호 101-12-143298 ㅣ 이메일: daesun@gmail.com"
+        }
+      />
+    </>
   );
 };
 
