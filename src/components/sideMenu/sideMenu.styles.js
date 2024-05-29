e165e0b7750167e413ffbb119b1177b3aa53a095
ltbhsf <kmukyj21@kookmin.ac.kr>
2024-05-29 23:03:18 +0900

    feat: change bg color & rm shadow from sideMenu

--- src/components/sideMenu/sideMenu.styles.js
+++ src/components/sideMenu/sideMenu.styles.js
@@ -9,11 +9,9 @@ export const Container = styled.div`
   width: 11.5rem;
   border-radius: 0.75rem;
   background-color: ${(props) => {
-    return props.$variant === true ? props.theme.color.white : "#F4F4F4";
+    return props.$variant === true && props.theme.color.grey._100 ;
   }};
   cursor: pointer;
-  box-shadow: ${(props) =>
-    props.$variant === true ? "0px 4px 12px rgba(0, 0, 0, 0.1)" : "none"};
 `;
 
 export const Section = styled.div`
