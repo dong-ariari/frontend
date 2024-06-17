d3cb424f8e4d624096e72eed92c85b4a64f57291
ltbhsf <kmukyj21@kookmin.ac.kr>
2024-06-17 21:48:22 +0900

    style: png to svg

--- src/data/components/sideMenu.js
+++ src/data/components/sideMenu.js
@@ -1,5 +1,8 @@
-import EyeIcon from "../../assets/images/eyes.png";
-import HeartIcon from "../../assets/images/green_heart.png";
+import EyeIcon from "../../assets/images/eyes.svg";
+import HeartIcon from "../../assets/images/green_heart.svg";
+import GlassIcon from '../../assets/images/glass.svg';
+import LetterIcon from '../../assets/images/letter.svg';
+import PencilIcon from '../../assets/images/pencil.svg';
 
 export const SIDEMENU_DATA = [
   {
@@ -12,14 +15,28 @@ export const SIDEMENU_DATA = [
   {
     id: 2,
     title: "전체 동아리",
-    imgPath: HeartIcon,
+    imgPath: GlassIcon,
     uri: "/offcampus",
     variant: false,
   },
   {
     id: 3,
     title: "나의 동아리",
-    imgPath: EyeIcon,
+    imgPath: HeartIcon,
+    uri: "/all",
+    variant: false,
+  },
+  {
+    id: 3,
+    title: "동아리 등록하기",
+    imgPath: LetterIcon,
+    uri: "/all",
+    variant: false,
+  },
+  {
+    id: 3,
+    title: "동아리 모집하기",
+    imgPath: PencilIcon,
     uri: "/all",
     variant: false,
   },
