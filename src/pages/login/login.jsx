813b28bd4c0fdd4d53373010274d232140b15c1c
yeoniii20 <commetoi_yeoni@naver.com>
2024-06-19 18:39:19 +0900

    feat: create login page

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,5 +1,18 @@
+import React from "react";
+import { REDIRECT_URI, REST_API_KEY } from "../api/common";
+
 const Login = () => {
-  return <></>;
+  const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+  const handleLogin = () => {
+    window.location.href = kakaoURL;
+    console.log("카카오 로그인을 클릭했습니다");
+  };
+
+  return (
+    <>
+      <button onClick={handleLogin}>카카오 로그인</button>
+    </>
+  );
 };
 
 export default Login;
