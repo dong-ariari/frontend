f741a1f21823712bb1416849f5ceb0ec04c68210
ltbhsf <kmukyj21@kookmin.ac.kr>
2024-06-28 21:17:06 +0900

    feat: search modal 기본 배치

--- src/pages/all/all.jsx
+++ src/pages/all/all.jsx
@@ -2,20 +2,71 @@ import { useState } from "react";
 import Cards from "../../components/cards/cards";
 import { DUMMY_CARD } from "../../data/dummy/cards";
 import MainLayout from "../../layouts/mainLayout/mainLayout";
+import Search from "../../components/search/search";
+import DropdownComponents from "../../components/dropdown/dropdownComponents";
 import * as S from "./all.styles";
-
+import { useDropDown, useDropDown2 } from "../../hooks/useDropdown";
+import SortTag from "../../components/tag/sortTag";
+import { SORT_TAG_DATA } from "../../data/components/sortTag";
 export default function All() {
+  // type: 전체 | 교내 | 연합
   const [type, setType] = useState(1);
   function onTypeChange(type) {
     setType(type);
   }
 
-  function onMiniSearchClick() {
-    console.log("mini search");
-  }
+  // searchActive: 검색 모달 활성화 여부
+  const [searchActive, setSearchActive] = useState(false);
+  // tag: 전체 | 교내 동아리 | 중앙 동아리 | 연합 동아리
+  const [tag, setTag] = useState();
+
+  // dropdowns
+  const { dropdown: depart, dropdown2: major } = useDropDown2(
+    "단과대학을 선택해주세요",
+    "학과를 선택해주세요"
+  );
+  const region = useDropDown("지역을 선택해주세요");
+  const field = useDropDown("분야를 선택해주세요");
 
   return (
-    <MainLayout onChipClick={(type) => onTypeChange(type)} onMiniSearchClick={onMiniSearchClick}>
+    <MainLayout
+      onChipClick={(type) => onTypeChange(type)}
+      onMiniSearchClick={() => {
+        setSearchActive(true);
+      }}
+    >
+      {searchActive && (
+        <>
+          <S.SearchModal>
+            <Search placeholder={"관심있는 동아리를 찾을 수 있어요"} />
+            <S.Filters>
+              <SortTag
+                data={SORT_TAG_DATA}
+                onSelectChange={(tagID) => {
+                  setTag(tagID);
+                }}
+              />
+              {/* 교내 */}
+              {tag === 1 && (
+                <DropdownComponents dropdown={depart} dropdown2={major} />
+              )}
+              {/* 중앙 */}
+              {tag === 2 && (
+                <DropdownComponents dropdown={field} dropdown2={null} />
+              )}
+              {/* 연합 */}
+              {tag === 3 && (
+                <DropdownComponents dropdown={field} dropdown2={region} />
+              )}
+            </S.Filters>
+          </S.SearchModal>
+          <S.Backdrop
+            onClick={() => {
+              setSearchActive(false);
+            }}
+          />
+        </>
+      )}
       <S.Container>
         <S.Title>
           모든 동아리({type === 1 ? "전체" : type === 2 ? "교내" : "연합"})
--- src/pages/all/all.styles.js
+++ src/pages/all/all.styles.js
@@ -12,3 +12,33 @@ export const Container = styled.div`
   flex-direction: column;
   margin-bottom: 4rem;
 `;
+
+export const Backdrop = styled.div`
+  background-color: rgba(0, 0, 0, 0.55);
+  position: absolute;
+  right: 0;
+  left: 0;
+  top: 20.25rem;
+  bottom: 0;
+  z-index: 100;
+`;
+
+export const SearchModal = styled.div`
+  position: absolute;
+  top: 5rem;
+  height: 13.25rem;
+  background-color: rgba(255, 255, 255, 0.9);
+  right: 0;
+  left: 0;
+  display: flex;
+  flex-direction: column;
+  padding-top: 2rem;
+`;
+
+export const Filters = styled.div`
+  margin-top: 1.5rem;
+  display: flex;
+  flex-direction: column;
+  gap: 0.75rem;
+  margin-left: 768px;
+`;
