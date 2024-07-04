fcba1ed94bbf0b7c94b6556d6561aa47e69b9c1b
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2024-07-04 15:44:02 +0900

    feat: add redirection route path

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,11 @@ import DefaultTheme from "./assets/styles/DefaultTheme";
 import All from "./pages/all";
 import Oncampus from "./pages/oncampus";
 import Offcampus from "./pages/offcampus";
-import ChipsComponent from "./components/chips/chipsComponent";
-import { CHIPS_DATA } from "./data/components/chips";
-import { Alert } from "./components/alert/alert";
-import { DUMMY_ALERT } from "./data/dummy/alert";
 import Mypage from "./pages/mypage";
 import Login from "./pages/login/login";
+import Redirection from "./pages/login/redirection";
+import Home from "./pages/home";
 
-// default theme 사용법
 const Test = styled.div`
   background-color: ${(props) => props.theme.color.grey._700};
   color: ${(props) => props.theme.color.white};
@@ -25,27 +22,15 @@ function App() {
     <Router>
       <GlobalStyle />
       <ThemeProvider theme={DefaultTheme}>
-        <div
-          style={{
-            backgroundColor: "pink",
-            paddingLeft: "100px",
-          }}
-        >
-          <Alert
-            data={DUMMY_ALERT}
-            onClick={() => {
-              console.log("clicked");
-            }}
-          />
-        </div>
         <Routes>
-          <Route path="/" element={<Test>Hello World</Test>} />
+          <Route path="/" element={<Home />} />
           <Route path="/all" element={<All />} />
           <Route path="/campus" element={<Oncampus />} />
           <Route path="/offCampus" element={<Offcampus />} />
           <Route path="/mypage" element={<Mypage />} />
           <Route path="/notification" element={<Notification />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/auth/kakao/callback" element={<Redirection />} />
         </Routes>
       </ThemeProvider>
     </Router>
