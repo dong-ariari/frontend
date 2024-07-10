b319674f8be4fc17894a6fb2edc9bfcba3fe1b17
yeoniii20 <commetoi_yeoni@naver.com>
2024-07-10 19:26:11 +0900

    feat: update kakao login

--- src/pages/login/redirection.jsx
+++ src/pages/login/redirection.jsx
@@ -1,15 +1,18 @@
-import React, { useEffect } from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const Redirection = () => {
   const navigate = useNavigate();
+  // const [code, setCode] = useState("");
 
   useEffect(() => {
     console.log("Redirection component mounted");
 
     const url = new URL(window.location.href);
     const code = url.searchParams.get("code");
-
+    fetchData(code);
+    // setCode(code);
     if (!code) {
       console.error("No authorization code found in URL.");
       return;
@@ -17,26 +20,36 @@ const Redirection = () => {
 
     console.log(`Authorization code: ${code}`);
 
-    // try {
-    //   const response = await axios.post("http://localhost:8080/api/getKakaoToken", { code });
-    //   const { access_token, refresh_token } = response.data;
-    //   if (!access_token || !refresh_token) {
-    //     throw new Error("Tokens not found in response");
-    //   }
-    //   console.log("Access Token:", access_token);
-    //   console.log("Refresh Token:", refresh_token);
-    //   localStorage.setItem("access_token", access_token);
-    //   localStorage.setItem("refresh_token", refresh_token);
-    // } catch (error) {
-    //   console.error("Failed to get token:", error);
-    // }
-
-    // 로컬 스토리지에 코드 저장 (임시)
     localStorage.setItem("auth_code", code);
 
     // 페이지 이동
-    navigate("/mypage");
+    // navigate("/mypage");
   }, [navigate]);
+
+  const fetchData = async (code) => {
+    const inputURL = `/auth/kakao/callback`;
+    try {
+      const response = await axios.get(inputURL, {
+        params: { code: code },
+        headers: {
+          Accept: "application/json",
+        },
+      });
+      console.log("response.data >>>", response.data);
+
+      // Assuming the response contains the JWT token
+      const { accessToken } = response.data;
+      console.log("123123", response);
+      if (accessToken) {
+        localStorage.setItem("jwt", accessToken);
+        console.log("JWT Token:", accessToken);
+      } else {
+        console.error("JWT token not found in the response");
+      }
+    } catch (error) {
+      console.error("Failed to fetch JWT:", error);
+    }
+  };
 
   return <div>로그인 중입니다...</div>;
 };
