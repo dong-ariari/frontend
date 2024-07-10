b6cca149b093101b205cf7c4f75bff62e74a61ba
yeoniii20 <commetoi_yeoni@naver.com>
2024-07-10 20:09:56 +0900

    fix: login api 수정

--- src/pages/login/redirection.jsx
+++ src/pages/login/redirection.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { API_URL } from "../../api/common";
 
 const Redirection = () => {
   const navigate = useNavigate();
@@ -11,7 +12,7 @@ const Redirection = () => {
 
     const url = new URL(window.location.href);
     const code = url.searchParams.get("code");
-    fetchData(code);
+    postData(code);
     // setCode(code);
     if (!code) {
       console.error("No authorization code found in URL.");
@@ -26,26 +27,31 @@ const Redirection = () => {
     // navigate("/mypage");
   }, [navigate]);
 
-  const fetchData = async (code) => {
-    const inputURL = `/auth/kakao/callback`;
+  const postData = async (code) => {
+    const inputURL = `${API_URL}/login?code=${code}`;
+    console.log(inputURL);
+    // const inputURL = `/login?code=${encodeURIComponent(code)}`;
     try {
-      const response = await axios.get(inputURL, {
-        params: { code: code },
-        headers: {
-          Accept: "application/json",
-        },
-      });
+      const response = await axios.post(
+        inputURL,
+        {},
+        {
+          headers: {
+            Accept: "application/json",
+          },
+        }
+      );
       console.log("response.data >>>", response.data);
 
-      // Assuming the response contains the JWT token
-      const { accessToken } = response.data;
-      console.log("123123", response);
-      if (accessToken) {
-        localStorage.setItem("jwt", accessToken);
-        console.log("JWT Token:", accessToken);
-      } else {
-        console.error("JWT token not found in the response");
-      }
+      // // Assuming the response contains the JWT token
+      // const { accessToken } = response.data;
+      // console.log("123123", response);
+      // if (accessToken) {
+      //   localStorage.setItem("jwt", accessToken);
+      //   console.log("JWT Token:", accessToken);
+      // } else {
+      //   console.error("JWT token not found in the response");
+      // }
     } catch (error) {
       console.error("Failed to fetch JWT:", error);
     }
