91b805364aa145ceb5cfe98aeb71d4fb95798210
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2024-07-25 17:58:41 +0900

    feat: test용 token 발급

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,14 +1,34 @@
 import React, { useState } from "react";
-import { REDIRECT_URI, REST_API_KEY } from "../../api/common";
+import axios from "axios";
+import { API_URL, REDIRECT_URI, REST_API_KEY } from "../../api/common";
 
 const Login = () => {
   const [loading, setLoading] = useState(false);
-  const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+  // const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 
   const handleLogin = () => {
     setLoading(true);
-    window.location.href = kakaoURL;
-    console.log("카카오 로그인을 클릭했습니다");
+    // window.location.href = kakaoURL;
+    // console.log("카카오 로그인을 클릭했습니다");
+    fetchData();
+  };
+
+  // 프로필 정보
+  const fetchData = async () => {
+    const inputURL = `/test/token`;
+    try {
+      const response = await axios.get(`${API_URL}${inputURL}`, {
+        headers: {
+          "Content-Type": "application/json; charset=utf-8",
+          "Access-Control-Allow-Origin": "*",
+        },
+      });
+      console.log(response.data);
+      const accessToken = response.data.accessTokaen;
+      console.log("accessToken >>>", accessToken);
+    } catch (error) {
+      console.error("에러:", error);
+    }
   };
 
   return (
